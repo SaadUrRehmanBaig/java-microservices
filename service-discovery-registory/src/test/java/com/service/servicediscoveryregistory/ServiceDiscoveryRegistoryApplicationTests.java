7bc220e79fd15f2a38059786bbff8ce6a7c6e0eb
saadbaig200 <saadbaig200@gmail.com>
2024-05-08 22:29:54 +0500

    implemented eureka server

---
+++ service-discovery-registory/src/test/java/com/service/servicediscoveryregistory/ServiceDiscoveryRegistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.servicediscoveryregistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryRegistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
